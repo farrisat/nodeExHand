434b5bba350d6e95ac171a4f386ec0df4a26c396
Alex F <farrisat1@gmail.com>
2019-10-20 14:36:18 -0700

    initial filesetup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES  ("chicken bacon burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger from Trashcan");
+INSERT INTO burgers (burger_name) VALUES ("Green Chili Best Burger");
